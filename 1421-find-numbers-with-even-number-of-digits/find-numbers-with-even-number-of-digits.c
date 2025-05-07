689a8109bfd98db6b67d09072786435fcbef3ec0
REETIK <reetikk77@gmail.com>
2025-05-08 03:27:29 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (10.70%) - LeetSync

---
+++ 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.c
@@ -0,0 +1,25 @@
+int findNumbers(int* nums, int numsSize) {
+    int count=0;
+    for(int i=0;i<numsSize;i++){
+        int cnt=0;
+        int num=nums[i];
+        if(num==0){
+            cnt=1;
+        }
+        else{
+            while(num>0){
+                num/=10;
+                cnt++;
+
+        }
+        }
+        if (cnt%2==0){
+            count+=1;
+        }
+        else{
+            continue;
+        }
+        
+    }
+    return count;
+}
