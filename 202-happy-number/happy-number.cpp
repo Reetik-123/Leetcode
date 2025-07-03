7975214115d8943e667f114195c570cd5ebb88b7
REETIK <reetikk77@gmail.com>
2025-07-03 23:14:52 +0530

    Time: 3 ms (19.74%) | Memory: 8.9 MB (6.64%) - LeetSync

---
+++ 202-happy-number/happy-number.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    long long int makeVec(int n) {
+    vector<int> v;
+    while (n > 0) {
+        int i = n % 10;
+        v.push_back(i);
+        n = n / 10;
+    }
+    long long int sum = 0;
+    for (int i = 0; i < v.size(); i++) {
+        sum += v[i] * v[i];
+    }
+    return sum;
+}
+
+bool isHappy(int n) {
+    set<long long int> seen;
+    long long int sums = n;
+    while (sums != 1 && seen.find(sums) == seen.end()) {
+        seen.insert(sums);
+        sums = makeVec(sums);
+    }
+    return sums == 1;
+}
+};
