6a5baf824fba30d61e664c495b1a5984e7b0cd53
REETIK <reetikk77@gmail.com>
2025-07-04 14:21:41 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (22.64%) - LeetSync

--- 202-happy-number/happy-number.cpp
+++ 202-happy-number/happy-number.cpp
@@ -1,25 +1,34 @@
 class Solution {
 public:
-    int makeVec(int n) {
-    vector<int> v;
-    while (n > 0) {
-        int i = n % 10;
-        v.push_back(i);
-        n = n / 10;
-    }
+    int f(int n){
     int sum = 0;
-    for (int i = 0; i < v.size(); i++) {
-        sum += v[i] * v[i];
+    while(n>0){
+        int digit = n%10;
+        sum += digit*digit;
+        n /= 10;
     }
     return sum;
-}
+    }
+//     int makeVec(int n) {
+//     vector<int> v;
+//     while (n > 0) {
+//         int i = n % 10;
+//         v.push_back(i);
+//         n = n / 10;
+//     }
+//     int sum = 0;
+//     for (int i = 0; i < v.size(); i++) {
+//         sum += v[i] * v[i];
+//     }
+//     return sum;
+// }
 
 bool isHappy(int n) {
     unordered_set<int> seen;
     int sums = n;
     while (sums != 1 && seen.find(sums) == seen.end()) {
         seen.insert(sums);
-        sums = makeVec(sums);
+        sums = f(sums);
     }
     return sums == 1;
 }
