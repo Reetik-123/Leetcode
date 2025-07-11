c679335f920f3228c88d688cc1137c01557dc011
REETIK <reetikk77@gmail.com>
2025-07-11 11:50:00 +0530

    Time: 5 ms (13.52%) | Memory: 28.4 MB (6.60%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -3,14 +3,9 @@ public:
     int majorityElement(vector<int>& nums) {
        unordered_map<int,int> hash;
        for(auto it : nums) hash[it]++;
-       int max=0;
-       int fs;
        for(auto it : hash){
-            if(it.second>max){
-                max=it.second;
-                fs=it.first;
-            }
+            if(it.second>nums.size()/2) return it.first;
        } 
-       return fs;
+       return -1;
     }
 };
