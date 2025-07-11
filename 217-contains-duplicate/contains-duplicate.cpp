71f5a38e10ceb554c4d24561c539c3839621bd49
REETIK <reetikk77@gmail.com>
2025-07-11 10:41:10 +0530

    Time: 68 ms (19.86%) | Memory: 93 MB (14.88%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -2,13 +2,9 @@ class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
         unordered_map<int,int> hash;
-        for(auto it : nums){
-            hash[it]++;
-        }
+        for(auto it : nums) hash[it]++;
         for(auto it : hash){
-            if(it.second>1){
-                return true;
-            }
+            if(it.second>1) return true;
         }
         return false;
     }
