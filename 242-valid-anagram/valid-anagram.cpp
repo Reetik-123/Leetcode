bf177378a0a6c26b06e9769c4a2fdd2d52f72254
REETIK <reetikk77@gmail.com>
2025-07-15 10:39:41 +0530

    Time: 7 ms (20.68%) | Memory: 10 MB (11.84%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,9 +1,20 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        if(s==t) return true;
-        else return false;
+        // sort(s.begin(),s.end());
+        // sort(t.begin(),t.end());
+        // if(s==t) return true;
+        // else return false;
+        unordered_map<char,int> hash;
+        for(auto it : s){
+            hash[it]++;
+        }
+        for(auto it : t){
+            hash[it]--;
+        }
+        for(auto it: hash){
+            if(it.second!=0) return false;
+        }
+        return true;
     }
 };
