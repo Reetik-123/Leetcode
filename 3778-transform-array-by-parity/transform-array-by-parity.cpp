1dd8bb79d8e504ffed107090e85706276b4ae0d2
REETIK <reetikk77@gmail.com>
2025-09-12 02:55:12 +0530

    Time: 0 ms (100.00%) | Memory: 28.3 MB (61.41%) - LeetSync

--- 3778-transform-array-by-parity/transform-array-by-parity.cpp
+++ 3778-transform-array-by-parity/transform-array-by-parity.cpp
@@ -1,13 +1,29 @@
 class Solution {
 public:
     vector<int> transformArray(vector<int>& nums) {
+        // for(int i=0;i<nums.size();i++){
+        //     if(nums[i]%2==0){
+        //         nums[i]=0;
+        //     }
+        //     else nums[i]=1;
+        // }
+        // sort(nums.begin(),nums.end());
+        // return nums;
+        int x=0,y=0;
         for(int i=0;i<nums.size();i++){
             if(nums[i]%2==0){
-                nums[i]=0;
+                x+=1;
             }
-            else nums[i]=1;
+            else y+=1;
         }
-        sort(nums.begin(),nums.end());
+        for(int i=0;i<x;i++){
+            nums[i]=0;
+        }
+        for(int i=x;i<nums.size();i++){
+            nums[i]=1;
+        }
         return nums;
+
+
     }
 };
