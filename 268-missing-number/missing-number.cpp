0a51847460e0502ac21a9e85a6d832ec18882a68
REETIK <reetikk77@gmail.com>
2025-11-06 14:54:40 +0530

    Time: 0 ms (100.00%) | Memory: 21.8 MB (69.86%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -1,25 +1,48 @@
 class Solution {
+
+private:
+
+    int ms2(vector<int>& arr){
+
+
+        int ele = 0 ;
+        int a = 0 ;
+
+
+        for(int i = 0 ; i < arr.size() ;i++){
+            a = a^arr[i];
+            ele = ele ^ (i+1);
+        }
+
+
+        return a ^ ele ;
+
+    }
+
+
 public:
     int missingNumber(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        // sort(nums.begin(),nums.end());
 
-        int si=0;
-        int ei=nums.size()-1;
-        int mid;
-        int ans;
-        while(si <= ei){
-            mid = si +(ei-si)/2;
-            if(nums[mid]==mid){
-                ans = mid;
-                si = mid+1;
-            }
-            else{
-                if(si == ei && nums[mid]!= mid){
-                    return mid;
-                }
-                ei = mid;
-            }
-        }
-        return ans+1;
+        // int si=0;
+        // int ei=nums.size()-1;
+        // int mid;
+        // int ans;
+        // while(si <= ei){
+        //     mid = si +(ei-si)/2;
+        //     if(nums[mid]==mid){
+        //         ans = mid;
+        //         si = mid+1;
+        //     }
+        //     else{
+        //         if(si == ei && nums[mid]!= mid){
+        //             return mid;
+        //         }
+        //         ei = mid;
+        //     }
+        // }
+        // return ans+1;
+
+        return ms2(nums);
     }
 };
