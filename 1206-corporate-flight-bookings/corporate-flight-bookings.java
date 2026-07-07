5cb92d442a845a8fe7b29d6daeee497cdc389647
REETIK <reetikk77@gmail.com>
2026-07-07 12:36:12 +0530

    Time: 3 ms (97.94%) | Memory: 84.5 MB (72.92%) - LeetSync

---
+++ 1206-corporate-flight-bookings/corporate-flight-bookings.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] corpFlightBookings(int[][] bookings, int n) {
+        int[] arr = new int[n];
+        for(int i=0;i<bookings.length;i++){
+            arr[bookings[i][0]-1]+=bookings[i][2];
+            if(bookings[i][1]<n){
+                arr[bookings[i][1]]-=bookings[i][2];
+            }
+        }
+        for(int i=1;i<n;i++){
+            arr[i]+=arr[i-1];
+        }
+        return arr;
+
+    }
+}
