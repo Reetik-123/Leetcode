024a3d4c729e57db3c98ee94e55cbb8d8541dc16
REETIK <reetikk77@gmail.com>
2026-08-12 10:09:13 +0530

    Time: 2 ms (88.81%) | Memory: 47.9 MB (63.31%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -3,16 +3,14 @@ class Solution {
         if(nums.length==1) return true;
         int ans=nums[0];
         for(int i=0;i<nums.length-1;i++){
-            if(i+nums[i]==nums.length-1) return true;
-            if(nums[i]<ans){
-                ans--;
-            }
-            else if(nums[i]==ans){
-                ans=nums[i];
-            }
-            else{
-                ans=nums[i];
-            }
+            // if(i+nums[i]==nums.length-1) return true;
+            // if(nums[i]<ans){
+            //     ans--;
+            // }
+            // else{
+            //     ans=nums[i];
+            // }
+            ans = Math.max(ans - 1, nums[i]);
             if(ans==0) return false;
         }
         return true;
