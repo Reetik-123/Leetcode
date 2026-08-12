f09bbb0643cd27ed49ede00ffeb8a16a2762532a
REETIK <reetikk77@gmail.com>
2026-08-12 11:30:28 +0530

    Time: 2 ms (88.81%) | Memory: 47.8 MB (81.72%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,18 +1,22 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        if(nums.length==1) return true;
-        int ans=nums[0];
-        for(int i=0;i<nums.length-1;i++){
-            // if(i+nums[i]==nums.length-1) return true;
+        int maxReach = 0;
+
+        for(int i = 0; i < nums.length; i++) {
+            if(i > maxReach) return false;
+
+            maxReach = Math.max(maxReach, i + nums[i]);
+
+            // if(maxReach >= nums.length - 1) return true;
+        }
+
+        return true;
+    }
+}
+           // if(i+nums[i]==nums.length-1) return true;
             // if(nums[i]<ans){
             //     ans--;
             // }
             // else{
             //     ans=nums[i];
             // }
-            ans = Math.max(ans - 1, nums[i]);
-            if(ans==0) return false;
-        }
-        return true;
-    }
-}
